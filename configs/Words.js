51baa6bc0f5a7195ecd8576da6288024494cfe44
Piyush Harjani <piyush303698@gmail.com>
2026-03-30 17:12:24 +0530

    after vite

--- configs/Words.js
+++
@@ -1,9 +0,0 @@
-export const WORDS = {
-    3: [
-        'ant', 'ape', 'bat', 'tax', 'bee', 'bar', 'bug', 'cat', 'dab', 'cow',
-        'cub', 'jog', 'dog', 'eel', 'any', 'fig', 'fin', 'fox', 'gum', 'hen',
-        'hut', 'hat', 'jar', 'key', 'log', 'map', 'nut', 'owl', 'pig', 'ram',
-        'rat', 'yak', 'web', 'jug', 'ink', 'zip', 'win', 'aim', 'joy', 'wow',
-        'yes', 'old', 'add', 'awe', 'bug', 'bed', 'big', 'sun', 'law', 'nap'
-    ],
-};
